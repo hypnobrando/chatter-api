79589b7c2758c422775e4354dd2521b8857544cc
brandoneprice31 <bprice@college.harvard.edu>
2017-08-04 19:30:58 -0700

    cleaned code

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from sanic import Sanic
 from users import users
 from chats import chats
 from messages import messages
+from config.config import Config
+
+config = Config()
 
 app = Sanic()
 app.blueprint(users)
 app.blueprint(chats)
 app.blueprint(messages)
 
-print("Starting up chatter api...")
-app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    print("Starting up chatter api in " + config.env + "...")
+    app.run(host="0.0.0.0", port=8080)
