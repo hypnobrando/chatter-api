513f2fe207bd30402f1fffe432efebf563550422
brandoneprice31 <bprice@college.harvard.edu>
2017-08-18 11:37:00 -0700

    changed badge count to 1

--- notifications/notify.py
+++ notifications/notify.py
@@ -11,6 +11,6 @@ class Notify:
         self.client = APNsClient('certs/sandbox.pem', use_sandbox=useSandbox, use_alternative_port=False)
 
     def sendMessages(self, apnTokens, message, custom):
-        payload = Payload(alert=message, sound="default", badge=10, custom=custom)
+        payload = Payload(alert=message, sound="default", badge=1, custom=custom)
         notifications = [Notification(token=token, payload=payload) for token in apnTokens if token != None]
         self.client.send_notification_batch(notifications, self.topic)
